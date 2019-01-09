64d16216fed2f032b6cf6de7d0c675f48f229593
Ewet <1922515598@qq.com>
2019-01-09 17:29:23 +0800

    d

--- sili_vue/src/views/test/VueCli/index.vue
+++ sili_vue/src/views/test/VueCli/index.vue
@@ -15,6 +15,11 @@
          <li>Node 版本要求Vue CLI 需要 Node.js 8.9 或更高版本 (推荐 8.11.0+)。你可以使用 nvm 或 nvm-windows 在同一台电脑中管理多个 Node 版本。</li>
          <li>我们推荐在运行 vue add 之前将项目的最新状态提交，因为该命令可能调用插件的文件生成器并很有可能更改你现有的文件。</li>
        </ul>
+       <h4>开发：</h4>
+       <p>注意事项：</p>
+       <ul>
+         <li>你会发现有 package.json 文件里的 browserslist 字段 (或一个单独的 .browserslistrc 文件)，指定了项目的目标浏览器的范围。这个值会被 @babel/preset-env 和 Autoprefixer 用来确定需要转译的 JavaScript 特性和需要添加的 CSS 浏览器前缀。</li>
+       </ul>
    </div>
 </transition>
 </template>
